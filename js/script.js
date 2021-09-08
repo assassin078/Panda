39b918b6ee3b8ed9378ad6addaaad531557af13e
assassin078 <tanvirakif123@gmail.com>
2021-09-08 08:53:00 +0600

    aaa

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// document.getElementById('subscribe').style.backgroundColor = 'steelblue'
+// const cards = document.getElementsByClassName('card');
+// for(const card of cards){
+//     card.style.backgroundColor = 'goldenrod';
+// }
+// function print(){
+//     console.log('clicked by onclick'); 
+// }
+
+// document.getElementById('click-button').addEventListener('click',function(){
+//     console.log('clicked by addeventlistener')
+// })
+
+// const bagBtns = document.getElementsByClassName('bag-btn') ;
+// for(const bagBtn of bagBtns){
+//     bagBtn.addEventListener('click',function(){
+//         bagBtn.parentElement.parentElement.parentElement.style.display = 'none' ;
+//     })
+// }
+// const emailField = document.getElementById('email-field');
+// emailField.addEventListener('keyup',function(){
+//     if(emailField.value == 'email'){
+//         document.getElementById('subscribe-btn').removeAttribute('disabled') ;
+//     }
+//     else{
+//         document.getElementById('subscribe-btn').setAttribute('disabled',true) ;
+//     }
+// })
+
+// document.getElementById('email-field').addEventListener('mouseenter' ,function(){
+//     document.getElementById('Subscribe-container').style.backgroundImage = 'url(images/wrecked.jpg)';
+// }) ;
+
